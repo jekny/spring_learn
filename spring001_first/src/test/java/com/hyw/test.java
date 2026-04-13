f4b6631d4dc21c046c21e75d5ae72fd653fd560c
jekny <2656803448@qq.com>
2026-04-13 18:33:23 +0800

    applicationContext的超级父接口BeanFactory（专门生产bean的工厂，底层用的工厂模式）

--- spring001_first/src/test/java/com/hyw/test.java
+++ spring001_first/src/test/java/com/hyw/test.java
@@ -3,6 +3,7 @@ package com.hyw;
 import com.hyw.mytest.bean.Car;
 import com.hyw.mytest.bean.User;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -19,15 +20,16 @@ public class test {
     public void test01(){
         //获取spring上下文对象（启动Spring的IoC容器）
         //当这行代码执行的时候会自动解析beans.xml文件，同时初始化配置文件中所有的bean，并放到Map集合当中
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml","applicationContext.xml");
+//        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml","applicationContext.xml");
+        BeanFactory factory = new ClassPathXmlApplicationContext("beans.xml","applicationContext.xml");
 
         //从Spring的IoC容器中通过id获取bean对象
-        User user1 = (User) applicationContext.getBean("User01");
+        User user1 = (User) factory.getBean("User01");
         System.out.println(user1);
         user1.study();
 
 //        Car carBean = (Car) applicationContext.getBean("CarBean");
-        Car carBean = applicationContext.getBean("CarBean", Car.class);
+        Car carBean = factory.getBean("CarBean", Car.class);
         System.out.println(carBean);
     }
 }
