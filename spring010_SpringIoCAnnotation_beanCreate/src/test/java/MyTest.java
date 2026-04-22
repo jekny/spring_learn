81534163913b71ffdc26b33914f6cbd774dbc8c3
jekny <2656803448@qq.com>
2026-04-22 18:52:38 +0800

    复制一份demo用于(Spring的IoC的注解式开发)-bean的声明(4种相同作用的注解)

---
+++ spring010_SpringIoCAnnotation_beanCreate/src/test/java/MyTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test;
+
+/**
+ * ClassName: MyTest
+ * Description:
+ *
+ * @Author jekny
+ * @Create 2026/4/22 18:51
+ * @Version 1.0
+ */
+public class MyTest {
+    @Test
+    public void test01(){
+        
+    }
+}
