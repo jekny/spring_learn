38e8c447c2af204ffe4b051036b9923e4752131f
jekny <2656803448@qq.com>
2026-04-27 20:55:55 +0800

    添加一个后置通知模拟日志的切面

--- spring014_AOP_AllAnnocation_TX/src/main/java/com/hyw/mytest/myaspect/MyAspect.java
+++ spring014_AOP_AllAnnocation_TX/src/main/java/com/hyw/mytest/myaspect/MyAspect.java
@@ -1,11 +1,17 @@
 package com.hyw.mytest.myaspect;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 /**
  * ClassName: MyAspect
@@ -20,6 +26,15 @@ import org.springframework.stereotype.Component;
 public class MyAspect {
     @Pointcut("execution(* com.hyw.mytest..*(..) )")
     public void myPointcut(){}
+
+    @After("myPointcut()")
+    public void MyAfter(JoinPoint proceedingJoinPoint){
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String format = dateTimeFormatter.format(now);
+        Signature signature = proceedingJoinPoint.getSignature();
+        System.out.println("用户在 "+format+" 执行了 "+signature.getDeclaringType()+signature.getName()+"方法");
+    }
 
     @Around("myPointcut()")
     public Object MyAround(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
