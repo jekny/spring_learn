ae3c621a4597178a9fddab5d70a78a16c4139089
jekny <2656803448@qq.com>
2026-05-19 18:54:57 +0800

    完成实现类

--- spring017_spring_mybatis_test3/src/main/java/com/hyw/mytest/service/AccountService2.java
+++ spring017_spring_mybatis_test3/src/main/java/com/hyw/mytest/service/AccountService2.java
@@ -1,5 +1,7 @@
 package com.hyw.mytest.service;
 
+import java.math.BigDecimal;
+
 /**
  * ClassName: AccountService2
  * Description:
@@ -9,5 +11,5 @@ package com.hyw.mytest.service;
  * @Version 1.0
  */
 public interface AccountService2 {
-    void transformal(String fromAct,String toAct);
+    void transformal(String fromAct, String toAct, BigDecimal balance);
 }
--- spring017_spring_mybatis_test3/src/main/java/com/hyw/mytest/service/impl/AccountServiceImpl2.java
+++ spring017_spring_mybatis_test3/src/main/java/com/hyw/mytest/service/impl/AccountServiceImpl2.java
@@ -1,7 +1,14 @@
 package com.hyw.mytest.service.impl;
 
+import com.hyw.mytest.entity.AccountEnd;
+import com.hyw.mytest.mapper.AccountMapper2;
 import com.hyw.mytest.service.AccountService2;
+import org.aspectj.apache.bcel.classfile.annotation.RuntimeAnnos;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
 
 /**
  * ClassName: AccountServiceImpl2
@@ -13,8 +20,22 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class AccountServiceImpl2 implements AccountService2 {
+    @Autowired
+    AccountMapper2 accountMapper2;
     @Override
-    public void transformal(String fromAct, String toAct) {
-
+    @Transactional
+    public void transformal(String from, String to, BigDecimal balance) {
+        AccountEnd fromAct = accountMapper2.selectAct(from);
+        if (fromAct.getBalance().compareTo(balance)<0){
+            throw  new  RuntimeException("余额不足");
+        }
+        fromAct.setBalance(fromAct.getBalance().subtract(balance));
+        AccountEnd toAct = accountMapper2.selectAct(to);
+        toAct.setBalance(toAct.getBalance().add(balance));
+        int count = accountMapper2.updateAct(fromAct);
+        count += accountMapper2.updateAct(toAct);
+        if (count!=2){
+            throw new RuntimeException("转账失败");
+        }
     }
 }
