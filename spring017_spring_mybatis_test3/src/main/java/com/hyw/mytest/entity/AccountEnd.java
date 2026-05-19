83e116d30e5c23d33f96295d99e2eb05d5177096
jekny <2656803448@qq.com>
2026-05-19 17:27:28 +0800

    引入相关的依赖

---
+++ spring017_spring_mybatis_test3/src/main/java/com/hyw/mytest/entity/AccountEnd.java
@@ -0,0 +1,17 @@
+package com.hyw.mytest.entity;
+
+import java.math.BigDecimal;
+
+/**
+ * ClassName: AccountEnd
+ * Description:
+ *
+ * @Author jekny
+ * @Create 2026/5/19 17:18
+ * @Version 1.0
+ */
+public class AccountEnd {
+    private int id;
+    private String actNo;
+    private BigDecimal balance;
+}
